914a116fb3a2c1cee88caf96d7d92452ed539b3c
Nikhitha <2400032181@kluniversity.in>
2026-04-01 15:18:09 +0530

    Update API base URL for production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-49a9.up.railway.app/api"
+  baseURL: "https://ecommerce-backend-pushed2-production.up.railway.app"
 });
 
 export default API;
